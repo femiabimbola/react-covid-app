e9499e61c9e85e0f2228a0a2ca86be98048fe3f3
femiabimbola <femiabimbola6@gmail.com>
2020-10-29 13:16:21 +0100

    added the app styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Cards, Charts, CountryPicker } from './components'
+import styles from './app.module.css'
 
 class App extends React.Component{
     
     render(){
         return (      
-        <div> 
-        <h1> The Covid App </h1> 
-        <Cards />
-        <CountryPicker />
-        <Charts />
+        <div className = {styles.container}> 
+          <h1> The Covid App </h1> 
+           <Cards />
+           <CountryPicker />
+           <Charts />
         </div>       
         )
     }

8f68b3faed532733c19b06622a4862d5e5bba2ec
femiabimbola <femiabimbola6@gmail.com>
2020-10-30 23:03:34 +0100

    cards almost done

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid} from '@material-ui/core';
+import CountUp from 'react-countup';
 import styles from './Cards.module.css'
 
-const Cards = ({confirmed, recovered, deaths, lastUpdate }) => {
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate }}) => {
   if(!confirmed){
     return 'Loaaading......'
   }
@@ -12,7 +13,8 @@ const Cards = ({confirmed, recovered, deaths, lastUpdate }) => {
             <Grid item component={Card}>
               <CardContent>
                 <Typography color="textSecondary" gutterBottom> Infected </Typography>
-                <Typography variant='h5'> {confirmed.value} </Typography>
+                <Typography variant='h5'> 
+                <CountUp start={0}  end={confirmed.value} duration= {3.5} separator="," /></Typography>
                 <Typography colour="textSecondary"> The real date </Typography>
                 <Typography variant="body2"> Number of active cases of Covid19 </Typography>
               </CardContent>
@@ -20,7 +22,8 @@ const Cards = ({confirmed, recovered, deaths, lastUpdate }) => {
             <Grid item component={Card}>
               <CardContent>
                 <Typography color="textSecondary" gutterBottom> Recovered </Typography>
-                <Typography variant='h5'> {recovered.value} </Typography>
+                {/*  Just check if data.recovered.value will work */}
+                <Typography variant='h5'> <CountUp start={0}  end={recovered.value} duration= {3.5} separator="," /> </Typography>
                 <Typography colour="textSecondary"> The real date </Typography>
                 <Typography variant="body2"> Number of recoveries from Covid19 </Typography>
               </CardContent>
@@ -28,7 +31,7 @@ const Cards = ({confirmed, recovered, deaths, lastUpdate }) => {
             <Grid item component={Card}>
               <CardContent>
                 <Typography color="textSecondary" gutterBottom> Deaths </Typography>
-                <Typography variant='h5'> T{deaths.value} </Typography>
+                <Typography variant='h5'> <CountUp start={0}  end={deaths.value} duration= {3.5} separator="," /> </Typography>
                 <Typography colour="textSecondary"> The real date </Typography>
                 <Typography variant="body2"> Number of deaths  caused by Covid19 </Typography>
               </CardContent>

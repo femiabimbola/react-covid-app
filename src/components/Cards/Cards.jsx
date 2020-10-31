7a3e0ef1c933d86e45e83363ba4af4b040a1dcb3
femiabimbola <femiabimbola6@gmail.com>
2020-10-31 11:32:46 +0100

    added card css

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,7 +11,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate }}) => {
   return (
         <div className = {styles.container}>
           <Grid container spacing={3} justify='center'>
-            <Grid item component={Card} className = {cx(styles.card, styles.infected)} >
+            <Grid item component={Card} xs ={12} md = {3} className = {cx(styles.card, styles.infected)} >
               <CardContent>
                 <Typography color="textSecondary" gutterBottom> Infected </Typography>
                 <Typography variant='h5'> 
@@ -20,7 +20,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate }}) => {
                 <Typography variant="body2"> Number of active cases of Covid19 </Typography>
               </CardContent>
             </Grid>
-            <Grid item component={Card} className = {cx(styles.card, styles.recovered)}>
+            <Grid item component={Card}  xs ={12} md = {3} className = {cx(styles.card, styles.recovered)}>
               <CardContent>
                 <Typography color="textSecondary" gutterBottom> Recovered </Typography>
                 {/*  Just check if data.recovered.value will work */}
@@ -29,7 +29,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate }}) => {
                 <Typography variant="body2"> Number of recoveries from Covid19 </Typography>
               </CardContent>
             </Grid>
-            <Grid item component={Card} className = {cx(styles.card, styles.deaths)}>
+            <Grid item component={Card} xs ={12} md = {3} className = {cx(styles.card, styles.deaths)}>
               <CardContent>
                 <Typography color="textSecondary" gutterBottom> Deaths </Typography>
                 <Typography variant='h5'> <CountUp start={0}  end={deaths.value} duration= {3.5} separator="," /> </Typography>

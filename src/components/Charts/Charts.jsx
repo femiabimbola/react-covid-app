fef359dc69670c7bcacf2436ad3d766bd900a4df
unknown <femiabimbola6@gmail.com>
2020-11-06 11:02:52 +0100

    styling the bar

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -37,8 +37,21 @@ const Charts = ({data, country}) => {
       data.confirmed ? (
         < Bar 
           data= {{
-            
-          }}/>
+            labels: ['Infected', 'Recovered', 'Deaths'],
+            datasets: [{
+              label: 'People',
+              backgroundColor: [
+                'rgba(0, 0, 255, 0.5)',
+                'rgba(0, 255, 0, 0.5)',
+                'rgba(255, 0, 0, 0.5)'
+              ]
+            }]
+          }}
+          options = {{
+            legend: {display: false},
+            title: {display: true, text:`Current state in ${country}`}
+          }}
+          />
       ) : null
     ) 
 
